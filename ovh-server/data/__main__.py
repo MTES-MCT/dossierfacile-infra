abf4c7de9d66d051b06d329d1d82084206964110
nicolasSagon <sagon.nicolas@gmail.com>
2025-07-31 15:27:38 +0200

    chore(data): add other bucket and users

--- ovh-server/data/__main__.py
+++ ovh-server/data/__main__.py
@@ -1,8 +1,13 @@
 import json
 
 import pulumi
+import pulumi_command as command
 import pulumi_ovh as ovh
 
+from data.configuration import get_storage_raw_file_name, get_storage_raw_file_minified_name, \
+    get_storage_watermark_doc_name, get_storage_full_pdf_name, get_storage_filigrane_name, get_user_api_tenant_name, \
+    get_user_pdf_generator_name, get_user_process_file_name, get_user_bo_name, get_user_task_scheduler_name, \
+    get_user_file_analysis_name, get_user_filigranefacile_name
 from data.data_stack_output import S3UserOutputData, DataStackOutput
 from lib.stack_output_util import stack_data_name
 
@@ -10,17 +15,19 @@ env = pulumi.get_stack()
 config = pulumi.Config()
 project_id = config.get("project_id")
 
-bucket_names = [
-    f"s-dossierfacile-{env}-raw-file",
-    # f"S_dossier_facile_{env}_raw_minified",
-    # f"S_dossier_facile_{env}_watermark_doc",
-    # f"S_dossier_facile_{env}_full_pdf",
-    # f"S_dossier_facile_{env}_filigrane"
+print()
+
+storage_names = [
+    get_storage_raw_file_name(env),
+    get_storage_raw_file_minified_name(env),
+    get_storage_watermark_doc_name(env),
+    get_storage_full_pdf_name(env),
+    get_storage_filigrane_name(env)
 ]
 
-buckets = {}
-for name in bucket_names:
-    buckets[name] = ovh.cloudproject.Storage(
+storages = {}
+for name in storage_names:
+    storages[name] = ovh.cloudproject.Storage(
         name,
         service_name=project_id,
         region_name="EU-WEST-PAR",
@@ -29,11 +36,13 @@ for name in bucket_names:
     )
 
 user_names = [
-    f"u_dossier_facile_{env}_ApiTenant",
-    # f"U_dossier_facile_{env}_PdfGenerator",
-    # f"U_dossier_facile_{env}_ProcessFile",
-    # f"U_dossier_facile_{env}_BO",
-    # f"U_dossier_facile_{env}_Filigrane"
+    get_user_api_tenant_name(env),
+    get_user_pdf_generator_name(env),
+    get_user_process_file_name(env),
+    get_user_bo_name(env),
+    get_user_task_scheduler_name(env),
+    get_user_file_analysis_name(env),
+    get_user_filigranefacile_name(env)
 ]
 
 users = {}
@@ -48,34 +57,46 @@ for name in user_names:
     )
 
     creds[name] = ovh.cloudproject.S3Credential(
-        f"C{name}",
+        f"c{name}",
         service_name=project_id,
         user_id=users[name].id
     )
 
 permissions = {
-    f"u_dossier_facile_{env}_ApiTenant": {
-        f"s-dossierfacile-{env}-raw-file": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
-        # f"S_dossier_facile_{env}_raw_minified": ["s3:GetObject", "s3:ListBucket"],
-        # f"S_dossier_facile_{env}_watermark_doc": ["s3:GetObject", "s3:ListBucket"],
-        # f"S_dossier_facile_{env}_full_pdf": ["s3:GetObject", "s3:ListBucket"]
+    get_user_api_tenant_name(env): {
+        get_storage_raw_file_name(env): ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
+        get_storage_raw_file_minified_name(env): ["s3:GetObject", "s3:ListBucket"],
+        get_storage_watermark_doc_name(env): ["s3:GetObject", "s3:ListBucket"],
+        get_storage_full_pdf_name(env): ["s3:GetObject", "s3:ListBucket"]
     },
-    # f"U_dossier_facile_{env}_PdfGenerator": {
-    #     f"S_dossier_facile_{env}_raw_file": ["s3:GetObject", "s3:ListBucket"],
-    #     f"S_dossier_facile_{env}_watermark_doc": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"],
-    #     f"S_dossier_facile_{env}_full_pdf": ["s3:PutObject", "s3:ListBucket"]
-    # },
-    # f"U_dossier_facile_{env}_ProcessFile": {
-    #     f"S_dossier_facile_{env}_raw_file": ["s3:GetObject", "s3:ListBucket"],
-    #     f"S_dossier_facile_{env}_raw_minified": ["s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
-    # },
-    # f"U_dossier_facile_{env}_BO": {
-    #     f"S_dossier_facile_{env}_raw_file": ["s3:GetObject", "s3:ListBucket"],
-    #     f"S_dossier_facile_{env}_watermark_doc": ["s3:GetObject", "s3:ListBucket"]
-    # },
-    # f"U_dossier_facile_{env}_Filigrane": {
-    #     f"S_dossier_facile_{env}_filigrane": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
-    # }
+    get_user_pdf_generator_name(env): {
+        get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
+        get_storage_watermark_doc_name(env): ["s3:PutObject", "s3:ListBucket"],
+        get_storage_full_pdf_name(env): ["s3:PutObject", "s3:ListBucket"],
+        get_storage_filigrane_name(env): ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+    },
+    get_user_process_file_name(env): {
+        get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
+        get_storage_raw_file_minified_name(env): ["s3:PutObject", "s3:ListBucket"]
+    },
+    get_user_bo_name(env): {
+        get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
+        get_storage_watermark_doc_name(env): ["s3:GetObject", "s3:ListBucket"]
+    },
+    get_user_task_scheduler_name(env): {
+        get_storage_raw_file_name(env): ["s3:DeleteObject", "s3:ListBucket"],
+        get_storage_raw_file_minified_name(env): ["s3:DeleteObject", "s3:ListBucket"],
+        get_storage_watermark_doc_name(env): ["s3:DeleteObject", "s3:ListBucket"],
+        get_storage_full_pdf_name(env): ["s3:DeleteObject", "s3:ListBucket"],
+        get_storage_filigrane_name(env): ["s3:DeleteObject", "s3:ListBucket"]
+    },
+    get_user_file_analysis_name(env): {
+        get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
+    },
+    get_user_filigranefacile_name(env): {
+        get_storage_filigrane_name(env): ["s3:PutObject", "s3:ListBucket", "s3:GetLifecycleConfiguration",
+                                             "s3:PutLifecycleConfiguration", "s3:GetObject"]
+    }
 }
 
 
@@ -83,7 +104,7 @@ def build_policy(bucket_user: str, rules: dict) -> str:
     statements = []
     for bucket_name, actions in rules.items():
         statements.append({
-            "Sid": f"{bucket_user}_{bucket_name}",
+            "Sid": f"{bucket_user}-{bucket_name}",
             "Effect": "Allow",
             "Action": actions,
             "Resource": [
@@ -96,22 +117,49 @@ def build_policy(bucket_user: str, rules: dict) -> str:
         "Statement": statements
     })
 
+
 for user, rule in permissions.items():
-    policy_json = pulumi.Output.all().apply(lambda _: build_policy(user, rule))
+    policy_json = build_policy(user, rule)
 
     ovh.cloudproject.S3Policy(
-        f"P{user}",
+        f"p{user}",
         service_name=project_id,
         user_id=users[user].id,
         policy=policy_json
     )
 
-buckets_names_output: list[pulumi.Output[str]] = list()
-for bucket in buckets:
-    buckets_names_output.append(buckets[bucket].name)
+# This is not possible for the moment with OVH terraform provider or pulumi provider to set lifecycle configuration
+# directly on the bucket, so we use a command to apply the lifecycle configuration.
+# The lifecycle configuration is stored in a file named filigrane_lifecycle.json in the same directory.
+lifecycle_endpoint_url = "https://s3.eu-west-par.io.cloud.ovh.net"
+
+filigrane_creds = creds[get_user_filigranefacile_name(env)]
+
+cmd = pulumi.Output.all(
+    filigrane_creds.access_key_id,
+    filigrane_creds.secret_access_key
+).apply(lambda keys:
+    f"AWS_ACCESS_KEY_ID={keys[0]} "
+    f"AWS_SECRET_ACCESS_KEY={keys[1]} "
+    f"aws s3api put-bucket-lifecycle-configuration "
+    f"--bucket {get_storage_filigrane_name(env)} "
+    f"--lifecycle-configuration file://filigrane_lifecycle.json "
+    f"--endpoint-url {lifecycle_endpoint_url} "
+    f"--region eu-west-par"
+)
+
+# Commande Pulumi qui applique la policy
+apply_lifecycle = command.local.Command(
+    f"clp{get_storage_filigrane_name(env)}",
+    create=cmd
+)
+
+storage_name_outputs: list[pulumi.Output[str]] = list()
+for storage in storages:
+    storage_name_outputs.append(storages[storage].name)
 
 s3_users: list[S3UserOutputData] = list()
 for user in creds:
     s3_users.append(S3UserOutputData(user, creds[user].access_key_id, creds[user].secret_access_key))
 
-pulumi.export(stack_data_name, DataStackOutput(buckets_names_output, s3_users))
+pulumi.export(stack_data_name, DataStackOutput(storage_name_outputs, s3_users))
