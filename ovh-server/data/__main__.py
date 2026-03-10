a10b8fec3ff3da66a90873eb3efb0f9bc9135728
nicolasSagon <sagon.nicolas@gmail.com>
2026-03-10 10:44:13 +0100

    feat: remove process file user

--- ovh-server/data/__main__.py
+++ ovh-server/data/__main__.py
@@ -76,10 +76,6 @@ permissions = {
         get_storage_filigrane_name(env): ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
         get_storage_raw_file_minified_name(env): ["s3:PutObject", "s3:ListBucket", "s3:DeleteObject"]
     },
-    get_user_process_file_name(env): {
-        get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
-        get_storage_raw_file_minified_name(env): ["s3:PutObject", "s3:ListBucket"]
-    },
     get_user_bo_name(env): {
         get_storage_raw_file_name(env): ["s3:GetObject", "s3:ListBucket"],
         get_storage_watermark_doc_name(env): ["s3:GetObject", "s3:ListBucket"]
